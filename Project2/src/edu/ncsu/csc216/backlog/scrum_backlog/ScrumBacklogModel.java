ac8b22b22bebf3b3a1f01ea45b20a1b04375bb65
jsmayo <Steven@192.168.0.6>
2017-03-23 18:50:14 -0400

    JavaDoc Applied to ScrumBacklogModel and TaskItemList

--- Project2/src/edu/ncsu/csc216/backlog/scrum_backlog/ScrumBacklogModel.java
+++ Project2/src/edu/ncsu/csc216/backlog/scrum_backlog/ScrumBacklogModel.java
@@ -4,53 +4,157 @@ import edu.ncsu.csc216.backlog.command.Command;
 import edu.ncsu.csc216.backlog.task.TaskItem;
 import edu.ncsu.csc216.backlog.task.TaskItem.Type;
 
+/** Concrete class that implements the Singleton design pattern.
+ * This class both maintains the TaskItemList and handles commands
+ * from the GUI, such as loading, saving, and creating new
+ * TaskItemLists.
+ * 
+ * @author Steven Mayo
+ */
 public class ScrumBacklogModel {
 	
+	/**
+	 * Constructor used for creating the ScrumBacklogModel
+	 * instance.
+	 */
 	public ScrumBacklogModel() {
 		
 	}
 	
+	/**
+	 * Static method used to retrieve the one and only instance
+	 * of ScrumBacklogModel.
+	 * @return ScrumBacklogModel Instance of ScrumBacklogModel
+	 */
 	public static ScrumBacklogModel getInstance() {
 		return new ScrumBacklogModel();
 	}
 	
+	/**
+	 * Saves the current TaskItemList as an XML file with the
+	 * specified filename parameter.
+	 * @param filename Name of the XML file to save the current 
+	 * TaskItemList to.
+	 * @throws IllegalArgumentException If the TaskItemList is null.
+	 */
 	public void saveTasksToFile(String filename) {
-		
+		try {
+			//TaskWriter.write(filename);
+		} catch(TaskIOException e) {
+			 throw new IllegalArgumentException();
+		}
 	}
 	
+	/**
+	 * Attempts to load a TaskItemList from the specified XML filename.
+	 * @param filename Filename used to read from and populate the list of
+	 * TaskItems.
+	 * @throws IllegalArgumentException Thrown if ScrumBacklogModel encounters
+	 * a TaskIOException when attempting to read from the specified file.
+	 * 
+	 */
 	public void loadTasksFromFile(String filename) {
-		
+		try {
+			//TaskWriter.read(filename);
+		} catch(TaskIOException e) {
+			 throw new IllegalArgumentException();
+		}
 	}
 	
+	/**
+	 * Creates a new TaskItemList.
+	 */
 	public void createNewTaskItemList() {
 	
 	}
 	
+	
+	/**
+	 * Provides information to the GUI by returning a 2D 
+	 * Object array that is used to populate the TaskListTable
+	 * with information.
+	 * @return 2D Object array containing 1 row  and 3 columns 
+	 * for every TaskItem returned.
+	 */
 	public Object[][] getTaskItemListAsArray() {
-		return new Object[0][0];
+		return new Object[0][3];
 	}
 	
+	
+	/**
+	 * Provides information to the GUI by returning a 2D 
+	 * Object array used to populate the TaskListTable
+	 * with TaskItems filtered by the owner parameter.
+	 * @param owner Name used to return information on TaskItems 
+	 * with only this specific value.
+	 * with 
+	 * @return 2D Object array containing a row for each TaskItem
+	 * owned by the specified parameter There is a column entry for each 
+	 * TaskItem's id number, state name, and title.
+	 * @throws IllegalArgumentException if the specified parameter is null.
+	 */
 	public Object[][] getTaskItemListByOwnerAsArray(String owner) {
+		if(owner == null) throw new IllegalArgumentException();
 		return new Object[0][0];
 	}
 	
+	
+	/**
+	 * Provides information to the GUI by returning a 2D 
+	 * Object array used to populate the TaskListTable
+	 * with TaskItems filtered by the owner parameter.
+	 * @param creator Name used to return information on TaskItems 
+	 * with only this specific creator name.
+	 * the 2D Object value. 
+	 * the TaskItem
+	 * @return 2D Object array containing a row for each TaskItem
+	 * created by the specified parameter. There is a column entry for each 
+	 * TaskItem's id number, state name, and title.
+	 * @throws IllegalArgumentException if the specified parameter is null.
+	 */
 	public Object[][] getTaskItemListByCreatorAsArray(String creator) {
-		return new Object[0][0];
+		if(creator == null) throw new IllegalArgumentException();
+		return new Object[0][3];
 	}
 	
+	/**
+	 * Returns the TaskItem associated with the specified unique ID number.
+	 * @param id Unique ID number of the TaskItem to return.
+	 * @return TaskItem associated with the provided unique ID number.
+	 */
 	public TaskItem getTaskItemById(int id) {
 		return null;
 	}
 	
+	/**
+	 * Executes the specified Command on the TaskItem Object corresponding
+	 * to the unique ID number parameter.
+	 * @param id Unique ID number of the TaskItem.
+	 * @param command Command to perform on the associated TaskItem.
+	 */
 	public void executeCommand(int id, Command command) {
 		
 	}
 	
+	/**
+	 * Deletes the TaskItem corresponding to the provided unique ID number.
+	 * @param id Unique ID number of the TaskItem to delete.
+	 */
 	public void deleteTaskItemById(int id) {
 		
 	}
 	
-	public void addTaskItemToList(String a, Type b, String c, String d) {
+	/**
+	 * Adds a TaskItem to the TaskItemList using the specified parameters for 
+	 * TaskItem title, Type, creator, and note value to 
+	 * @param title Title of the TaskItem.
+	 * @param type Type of the TaskItem.
+	 * @param creator Creator of the TaskItem.
+	 * @param note Note designated for the TaskItem.
+	 */
+	public void addTaskItemToList(String title, Type type, String creator, String note) {
+		TaskItem taskItem = new TaskItem(title, type, creator, note);
+		tasks.addTaskItem(title, type, creator, note);
 		
 	}
 }
--- Project2/src/edu/ncsu/csc216/backlog/scrum_backlog/TaskItemList.java
+++ Project2/src/edu/ncsu/csc216/backlog/scrum_backlog/TaskItemList.java
@@ -8,48 +8,110 @@ import edu.ncsu.csc216.backlog.command.Command;
 import edu.ncsu.csc216.backlog.task.TaskItem;
 import edu.ncsu.csc216.backlog.task.TaskItem.Type;
 
+/**
+ * Concrete class used to manage all tasks during Scrum Backlog's
+ * execution. The TaskItemList maintains a List of TaskItem objects
+ * and has support for adding, removing, searching for, updating, and 
+ * filtering TaskItems.
+ * 
+ * @author Steven Mayo
+ */
 public class TaskItemList {
 
 	ArrayList<TaskItem> tasks;
 	static final int INITIAL_COUNTER_VALUE = 1;
 	
+	/**
+	 * Constructor for the TaskItemList object.
+	 */
 	public TaskItemList() {
 	}
 	
+	/**
+	 * Removes the contencts of the current TaskItemList.
+	 */
 	private void emptyList() {
+		//stuff
+	}
+	
+	/**
+	 * Adds a TaskItem to the TaskItemList using the specified 
+	 * parameters for TaskItem: title, Type, creator, and note.
+	 * @param title Title of the TaskItem.
+	 * @param type Type of the TaskItem.
+	 * @param creator Creator of the TaskItem.
+	 * @param note Note designated for the TaskItem.
+	 */
+	public void addTaskItemToList(String title, Type type, String creator, String note) {
 		
 	}
 	
-	public int addTaskItem(String a, Type b, String c, String d) {
-		return 1;
-	}
-	
+	/**
+	 * Add's a list of Tasks to the current TaskItemList.
+	 * @param List of Task's to add to the TaskItemList.
+	 */
 	public void addXMLTasks(List<Task> tasklist) {
 		
 	}
 	
+	/**
+	 * Getter method that returns the current list of TaskItems.
+	 * @return The list of TaskItems.
+	 */
 	public List<TaskItem> getTaskItems() {
 		return tasks;
 	}
 	
+	/**
+	 * Getter method that returns a filtered list of TaskItems. The
+	 * owner is used as means for filtering the current TaskItemList.
+	 * @param owner Owner of the TaskItem.
+	 * @return List of TaskItem objects that are filtered by the TaskItem
+	 * owner name.
+	 */
 	public List<TaskItem> getTaskItemsByOwner(String owner) {
 		return new ArrayList<TaskItem>();
 		
 	}
 	
+	/**
+	 * Getter method that returns a filtered list of TaskItems. The
+	 * Task creator is used as means for filtering the current TaskItemList.
+	 * @param owner Creator of the TaskItem.
+	 * @return List of TaskItem objects that are filtered by the TaskItem
+	 * creator name.
+	 */
 	public List<TaskItem> getTaskItemsByCreator(String creator) {
 		return new ArrayList<TaskItem>();
 	}
 	
+	/**
+	 * Getter method that searches for, and retrieves, the TaskItem 
+	 * associated with the provided unique ID number.
+	 * @param id Unique ID number of the TaskItem to retrieve.
+	 * @return TaskItem associated with the provided unique ID number.
+	 * creator name.
+	 */
 	public TaskItem getTaskItemById(int id) {
 		return tasks.get(id);
 	}
 	
+	/**
+	 * Updates a TaskItem in the list through execution of the 
+	 * passed in Command parameter.
+	 * @param id The Unique ID number of the TaskItem.
+	 * @param command Command to perform on the TaskItem.
+	 */
 	public void executeCommand(int id, Command command) {
 		
 	}
 	
-	public void deleteTAskItemById(int id) {
+	/**
+	 * Deletes the TaskItem in the TaskItemList using the specified
+	 * unique ID parameter.
+	 * @param id Unique ID number of the TaskItem to delete.
+	 */
+	public void deleteTaskItemById(int id) {
 		
 	}
 }
