0da5ff9257a5a5e35a053578766a8b6f8fc6c350
jsmayo <Steven@192.168.0.6>
2017-03-24 06:14:28 -0400

    Added test source directory.
    Also added a blank test class for commandTest

---
+++ Project2/test/edu/ncsu/csc216/backlog/command/CommandTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.backlog.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Steven
+ */
+public class CommandTest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
